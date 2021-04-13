b19fc490491420e9a77bfad6bcbc7fd201886347
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-13 15:46:25 -0400

    commit tree 27c7999437b520775aa6f0b0121a5e979fd344c6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+  let newArray = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(number) {
+  let count = 0;
+  while (number >= count) {
+    console.log(count)
+    count++
+  }
+}
